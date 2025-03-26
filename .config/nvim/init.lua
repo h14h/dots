d3d7907b1bfbeb7197dc2c9e9e0f3d81da55585d
Henry Firth <contact@henrynelsonfirth.com>
2025-03-26 16:26:12 -0500

    Set up some basic mini.nvim things

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,5 @@
+-- [[ Global Settings ]]
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -8,6 +10,8 @@ vim.g.have_nerd_font = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
+
+vim.opt.showmode = false
 
 vim.opt.breakindent = true
 vim.opt.undofile = true
@@ -28,7 +32,7 @@ vim.opt.inccommand = "split"
 
 vim.opt.cursorline = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 3
 
 vim.opt.confirm = true
 
@@ -107,63 +111,40 @@ require("lazy").setup({
       },
     },
   },
-  {
-    'nvim-telescope/telescope.nvim',
-    event = 'VimEnter',
-    branch = '0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-ui-select.nvim' },
-    },
+  { -- Collection of various small independent plugins/modules
+    'echasnovski/mini.nvim',
     config = function()
-      require('telescope').setup {
-            extensions = {
-              ['ui-select'] = {
-                require('telescope.themes').get_dropdown(),
-              },
-            },
-          }
+      require('mini.ai').setup { n_lines = 500 }
+      require('mini.surround').setup()
+      require('mini.icons').setup()
 
-          pcall(require('telescope').load_extension, 'ui-select')
+      require('mini.pick').setup()
+      require('mini.extra').setup()
 
-          -- See `:help telescope.builtin`
-          local builtin = require 'telescope.builtin'
-          vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-          vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-          vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-          vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-          vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-          vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-          vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-          vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-          vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-          vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      local builtin = MiniPick.builtin
+      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sh', builtin.help, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sf', builtin.files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sw', builtin.grep, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sg', builtin.grep_live, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
 
-          -- Slightly advanced example of overriding default behavior and theme
-          vim.keymap.set('n', '<leader>/', function()
-            -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-            builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-              winblend = 10,
-              previewer = false,
-            })
-          end, { desc = '[/] Fuzzily search in current buffer' })
+      -- TODO: There's probably some cool stuff you can build with this
+      -- vim.keymap.set('n', '<leader>sx', builtin.cli, { desc = '[S]earch E[x]ecutable Output' })
 
-          -- It's also possible to pass additional configuration options.
-          --  See `:help telescope.builtin.live_grep()` for information about particular keys
-          vim.keymap.set('n', '<leader>s/', function()
-            builtin.live_grep {
-              grep_open_files = true,
-              prompt_title = 'Live Grep in Open Files',
-            }
-          end, { desc = '[S]earch [/] in Open Files' })
+      local statusline = require('mini.statusline')
+      statusline.setup { use_icons = vim.g.have_nerd_font }
 
-          -- Shortcut for searching your Neovim configuration files
-          vim.keymap.set('n', '<leader>sn', function()
-            builtin.find_files { cwd = vim.fn.stdpath 'config' }
-          end, { desc = '[S]earch [N]eovim files' })
-        end,
-      },
-    })
+      -- You can configure sections in the statusline by overriding their
+      -- default behavior. For example, here we set the section for
+      -- cursor location to LINE:COLUMN
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
+    end,
+  }
+})
 
-    -- The line beneath this is called `modeline`. See `:help modeline`
-    -- vim: ts=2 sts=2 sw=2 et
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
