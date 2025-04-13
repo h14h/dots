84bd7a18dc185ed4299a317a9bda3fa1a4d07771
Henry Firth <contact@henrynelsonfirth.com>
2025-04-13 14:20:28 -0500

    Add file picker keymap

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -18,6 +18,8 @@ return {
 			local statusline = require("mini.statusline")
 			statusline.setup({ use_icons = vim.g.have_nerd_font })
 
+			vim.keymap.set("n", "<leader>-", MiniFiles.open, { desc = "Open File Picker" })
+
 			-- You can configure sections in the statusline by overriding their
 			-- default behavior. For example, here we set the section for
 			-- cursor location to LINE:COLUMN
